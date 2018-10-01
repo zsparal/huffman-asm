2ca0feabc3cf9627d4f689123eaa109fcb0bee08
Gustorn <gustorn@gmail.com>
2018-10-01 21:23:13 +0200

    Use a stack allocated priority queue

--- main.s
+++ main.s
@@ -11,24 +11,24 @@
   branch_fmt: .string "BR: %d\n"
   text:       .string "bibbity bobbity"
 
-  .equ bitstr_length,   32
-  .equ bitstr_size,     40
-  .equ codebook_size,   256 * bitstr_size
+  .equ bitstr_length,    32
+  .equ bitstr_size,      40
+  .equ codebook_size,    256 * bitstr_size
 
-  .equ tree_left,       0
-  .equ tree_right,      8
-  .equ tree_count,      16
-  .equ tree_value,      20
-  .equ tree_size,       24
+  .equ tree_left,        0
+  .equ tree_right,       8
+  .equ tree_count,       16
+  .equ tree_value,       20
+  .equ tree_size,        24
 
-  .equ heap_data,       0
-  .equ heap_len,        8
-  .equ heap_cap,        12
-  .equ heap_size,       16
+  .equ heap_len,         0
+  .equ heap_data,        4
+  .equ heap_size,        512 * 8 + 16 # 512 ptrs + 4 byte length + 12 byte padding
+  .equ counts_size,      256 * 4
 
 .section .text
   .global main
-  .extern printf, calloc, malloc, realloc, puts
+  .extern printf, calloc, malloc, memset, puts
 
 main:
   push   r12
@@ -124,17 +124,18 @@ generate_codebook_recurse_done:
 generate_tree:
   push   r12
   push   r13
-  sub    rsp, 1056                            # 1024 bytes for the char counts, 16 bytes for the heap, 8 extra bytes to align to 16-bytes
-  mov    r12, rdi                             # Save the text to a register that won't be clobbered
-  xor    rax, rax                             # Create the 256-length count array for each possible "char" value
-  mov    rcx, 128
-  lea    rdi, QWORD PTR [rsp + 16]
-  rep    stosq
+  sub    rsp, 4176                            # 1024 bytes for the char counts, 4 bytes for heap length, 4096 bytes for the heap, 12 byte padding
+  mov    r12, rdi                             # Save the original text so it doesn't get clobbered
+  mov    rdi, rsp                             # Zero out the character counts and the heap length
+  xor    rsi, rsi
+  mov    rdx, 1040
+  call   memset
+  xor    rax, rax
 generate_tree_count_chars:
   mov    al, BYTE PTR [r12]
   test   al, al
   jz     generate_tree_construct_heap
-  inc    DWORD PTR [rsp + 16 + 4*rax]
+  inc    DWORD PTR [rsp + 4*rax]
   inc    r12
   jmp    generate_tree_count_chars
 generate_tree_construct_heap:
@@ -144,7 +145,7 @@ generate_tree_construct_heap:
   cmp    r12, 0                               # Check if we reached zero (on subsequent iterations "dec" sets the correct flag)
 generate_tree_leaves:
   jl     generate_tree_branches               # If not then it's time to generate the branches
-  mov    r13d, DWORD PTR [rsp + 16 + 4*r12]   # Load the count at the ith position
+  mov    r13d, DWORD PTR [rsp + 4*r12]        # Load the count at the ith position
   test   r13d, r13d                           # And check if it's zero
   jz     generate_tree_leaves_counters        # If it is we can skip this iteration
   mov    rdi, 1                               # If not, we need to allocate a new leaf node
@@ -152,19 +153,19 @@ generate_tree_leaves:
   call   calloc
   mov    DWORD PTR [rax + tree_value], r12d   # Save the value and the count to the tree
   mov    DWORD PTR [rax + tree_count], r13d
-  mov    rdi, rsp                             # Then push it onto the heap
+  lea    rdi, [rsp + counts_size]             # Then push it onto the heap
   mov    rsi, rax
   call   heap_push
 generate_tree_leaves_counters:
   dec    r12                                  # Decrement the loop counter and start over
   jmp    generate_tree_leaves
 generate_tree_branches:
-  cmp    DWORD PTR [rsp + heap_len], 1        # Check if there are still at least two elements in the heap
+  cmp    DWORD PTR [rsp + counts_size], 1     # Check if there are still at least two elements in the heap
   jle    generate_tree_done                   # If not, we're done
-  mov    rdi, rsp                             # Get the left child
+  lea    rdi, [rsp + counts_size]             # Get the left child
   call   heap_pop
   mov    r12, rax
-  mov    rdi, rsp                             # Get the right child
+  lea    rdi, [rsp + counts_size]             # Get the right child
   call   heap_pop
   mov    r13, rax
   mov    rdi, tree_size                       # Create the new tree node, the pointer to it will be in rax
@@ -174,18 +175,14 @@ generate_tree_branches:
   mov    QWORD PTR [rax + tree_left], r12     # Save the new node's fields: left, right, count (leave value unititialized, it shouldn't be used with branch nodes)
   mov    QWORD PTR [rax + tree_right], r13
   mov    DWORD PTR [rax + tree_count], ecx
-  mov    rdi, rsp                             # Add the branch to the heap
+  lea    rdi, [rsp + counts_size]             # Add the branch to the heap
   mov    rsi, rax
   call   heap_push
   jmp    generate_tree_branches
 generate_tree_done:
-  mov    rdi, rsp                             # The tree's root will be in rax after the pop
+  lea    rdi, [rsp + counts_size]             # The tree's root will be in rax after the pop
   call   heap_pop
-  mov    r12, rax
-  mov    rdi, [rsp]                           # Free the heap
-  call   free
-  mov    rax, r12                             # And return the tree root
-  add    rsp, 1056
+  add    rsp, 4176
   pop    r13
   pop    r12
   ret
@@ -193,26 +190,7 @@ generate_tree_done:
 # rdi - heap ptr
 # rsi - tree ptr
 heap_push:
-  mov    rax, QWORD PTR [rdi + heap_data]     # We load the heap's data ptr, length and capacity to the respective registers
-  mov    edx, DWORD PTR [rdi + heap_cap]      # Load the current capacity
-  cmp    DWORD PTR [rdi + heap_len], edx      # If length == capacity we have to grow the heap
-  jne    heap_push_add
-heap_push_grow:
-  mov    r8d, 4                               # Load the initial capacity
-  lea    edx, [edx + edx]                     # Calculate 2 * current capacity
-  test   eax, eax                             # Check if the data ptr is null
-  cmovne r8d, edx                             # And if it wasn't, update the capacity to the doubled one
-  mov    DWORD PTR [rdi + heap_cap], r8d      # Save the new capacity
-  push   rdi                                  # Save the registers we don't want realloc to clobber   
-  push   rsi
-  mov    rdi, rax                             # Set up the parameters for realloc: data ptr, target size
-  mov    rsi, r8
-  shl    rsi, 3                               # Realloc expects the size in bytes but we store them as a count. Multiply by the size of a tree pointer
-  call   realloc                              # After this point the correct data ptr (either the original or the realloc'ed one) will be in rax
-  pop    rsi                                
-  pop    rdi
-  mov    QWORD PTR [rdi + heap_data], rax     # Save the new data ptr
-heap_push_add:
+  lea    rax, QWORD PTR [rdi + heap_data]     # We load the heap's data ptr and length to the respective registers
   mov    ecx, DWORD PTR [rdi + heap_len]      # Load the current length
   lea    edx, [ecx + 1]                       # First, calculate the new length (length + 1)
   mov    DWORD PTR [rdi + heap_len], edx      # Then save it
@@ -229,8 +207,8 @@ heap_push_sift_up:
   mov    esi, DWORD PTR [r10 + tree_count]    # Load the current tree's count
   cmp    DWORD PTR [r11 + tree_count], esi    # If parent count <= current count
   jle    heap_push_done                       # Then we're done
-  mov    DWORD PTR [r8d], r11d                # Otherwise swap the two elements
-  mov    DWORD PTR [r9d], r10d
+  mov    QWORD PTR [r8], r11                  # Otherwise swap the two elements
+  mov    QWORD PTR [r9], r10 
   mov    rcx, rdx
   jmp    heap_push_sift_up
 heap_push_done:
@@ -242,7 +220,7 @@ heap_pop:
   mov    r8d, DWORD PTR [rdi + heap_len]      # Load the heap's length 
   test   r8d, r8d                             # If it's 0 then the heap's empty
   jz     heap_empty
-  mov    rdx, [rdi + heap_data]               # Get the heap's data ptr
+  lea    rdx, [rdi + heap_data]               # Get the heap's data ptr
   mov    rax, QWORD PTR [rdx]                 # The return value will be the tree's current root
   lea    r8d, [r8d - 1]                       # Calculate the new length
   mov    DWORD PTR [rdi + heap_len], r8d      # And save it
